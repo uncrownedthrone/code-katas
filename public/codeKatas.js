3037f1b0777547bdb4db18d1447cd876bcff7e03
Justin Swiger <justin.swiger@me.com>
2020-02-26 19:51:41 -0500

    added proforma codekatas

--- public/codeKatas.js
+++ public/codeKatas.js
@@ -15,3 +15,17 @@ function getRealFloor(n) {
 function solution(str, ending) {
   return str.endsWith(ending)
 }
+
+// Two Oldest Ages
+
+const twoOldestAges = ages => ages.sort((a, b) => a - b).slice(-2)
+
+// No zeroes for heros
+
+function noBoringZeros(n) {
+  let result = String(n)
+  while (result.endsWith(0)) {
+    result = result.substring(0, result.length - 1)
+  }
+  return Number(result)
+}
